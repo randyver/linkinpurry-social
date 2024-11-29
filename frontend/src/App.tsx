e64fb47c190ac540def24e8157aa028f3d73cbef
salsbiila <13522062@std.stei.itb.ac.id>
2024-11-29 14:35:00 +0700

    fix: profile ui, navbar; feat: sticky footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
-// src/App.tsx
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/navbar";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
@@ -9,23 +8,50 @@ import { Toaster } from "react-hot-toast";
 import Connections from "./pages/Connections";
 import Profile from "./pages/Profile";
 import Footer from "./components/footer";
+import StickyFooter from "./components/sticky-footer";
 
 function App() {
   return (
     <Router>
+      <Layout />
+    </Router>
+  );
+}
+
+function Layout() {
+  const location = useLocation();
+
+  const stickyFooterPaths = [
+    "/userlist",
+  ];
+
+  const isStickyFooter = stickyFooterPaths.some((path) =>
+    matchPath(path, location.pathname)
+  );
+
+  return (
+    <>
       <Navbar />
       <Toaster />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/userlist" element={<Users/>} />
+        <Route path="/userlist" element={<Users />} />
         <Route path="/connections/user/:userId" element={<Connections />} />
         <Route path="/profile/:userId" element={<Profile />} />
       </Routes>
-      <Footer />
-    </Router>
+      {isStickyFooter ? <StickyFooter /> : <Footer />}
+    </>
   );
+}
+
+// just in case any dynamic routes are included in the stickyFooterPaths array
+function matchPath(pattern: string, pathname: string): boolean {
+  const regex = new RegExp(
+    "^" + pattern.replace(/:[^\s/]+/g, "([^/]+)") + "$"
+  );
+  return regex.test(pathname);
 }
 
 export default App;
