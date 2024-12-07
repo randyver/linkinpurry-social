a10dd3a70310861d89c85346f8ba4c0764be7517
randyver <randyverdian99@gmail.com>
2024-12-07 13:55:45 +0700

    styling: responsive feed page

--- frontend/src/components/profile-picture.tsx
+++ frontend/src/components/profile-picture.tsx
@@ -4,7 +4,7 @@ type ProfilePictureProps = {
 
 export const ProfilePicture = ({ src }: ProfilePictureProps) => {
   return (
-    <div className='lg rounded-full overflow-hidden border-4 border-white w-24 h-24'>
+    <div className='lg rounded-full overflow-hidden border-4 border-white w-24 h-24 md:w-20 md:h-20'>
       <img
         src={src}
         alt="Profile"
