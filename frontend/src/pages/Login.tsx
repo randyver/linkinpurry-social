611276b1294579e4dabb91962b64b4d76dc6cde0
salsbiila <salsabiilabslmh@gmail.com>
2024-12-09 17:57:17 +0700

    refactor: adding hide and unhide password

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { z } from "zod";
@@ -20,6 +21,7 @@ import {
 } from "../components/ui/card";
 import { toast } from "react-hot-toast";
 import { Link } from "react-router-dom";
+import { Eye, EyeOff } from "lucide-react";
 
 const loginSchema = z.object({
   email: z.string().email("Email is required"),
@@ -37,72 +39,8 @@ export default function Login() {
       password: "",
     },
   });
+  const [showPassword, setShowPassword] = useState(false);
 
-  const requestNotificationPermission = async () => {
-    console.log("Current Notification Permission:", Notification.permission);
-    if ("Notification" in window && Notification.permission !== "granted") {
-      console.log("Requesting notification permission");
-      try {
-        const permission = await Notification.requestPermission();
-        if (permission !== "granted") {
-          console.error("Notification permission denied");
-        }
-      } catch (error) {
-        console.error("Failed to request notification permission:", error);
-      }
-    }
-  };
-
-  const getVapidKey = async (): Promise<string> => {
-    try {
-      const response = await fetch("http://localhost:3000/api/vapid-key");
-      if (!response.ok) {
-        throw new Error("Failed to fetch VAPID key");
-      }
-      const { vapidKey } = await response.json();
-      return vapidKey;
-    } catch (error) {
-      console.error("Error fetching VAPID key:", error);
-      throw error;
-    }
-  };
-  
-
-  const registerPushSubscription = async () => {
-    console.log("Registering push subscription");
-    if ("serviceWorker" in navigator) {
-      try {
-        const vapidKey = await getVapidKey();
-        const registration = await navigator.serviceWorker.ready;
-  
-        const subscription = await registration.pushManager.subscribe({
-          userVisibleOnly: true,
-          applicationServerKey: vapidKey,
-        });
-
-        console.log(subscription);
-        
-        const response = await fetch("http://localhost:3000/api/save-push-subscription", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          credentials: "include",
-          body: JSON.stringify(subscription),
-        });
-  
-        if (!response.ok) {
-          throw new Error("Failed to save subscription on the server");
-        }
-  
-        console.log("Push subscription registered successfully");
-      } catch (error) {
-        console.error("Failed to register push subscription:", error);
-      }
-    }
-  };
-  
-  
   const onSubmit: SubmitHandler<LoginSchema> = async (data) => {
     toast.loading("Logging in...");
     try {
@@ -122,13 +60,9 @@ export default function Login() {
         toast.error(result.message || "Login failed. Please check your credentials.");
         return;
       }
-  
+
       toast.dismiss();
       toast.success(`Welcome back!`);
-      await requestNotificationPermission();
-      if (Notification.permission === "granted") {
-        await registerPushSubscription();
-      }
       navigate("/home");
     } catch (error) {
       toast.dismiss();
@@ -136,7 +70,6 @@ export default function Login() {
     }
   };
   
-
   return (
     <div className="flex justify-center items-center min-h-screen bg-wbd-background text-wbd-text px-4 sm:px-6 lg:px-8">
       <Card className="w-full max-w-lg md:max-w-md bg-wbd-secondary shadow-lg">
@@ -161,6 +94,18 @@ export default function Login() {
                         {...field}
                         className="border-wbd-tertiary focus:ring-wbd-highlight focus:border-wbd-highlight"
                       />
+                      <button
+                        type="button"
+                        className="absolute right-3 top-1/2 transform -translate-y-1/2"
+                        onClick={() => setShowPassword(!showPassword)}
+                        aria-label="Toggle Password Visibility"
+                      >
+                        {showPassword ? (
+                          <EyeOff size={20} />
+                        ) : (
+                          <Eye size={20} />
+                        )}
+                      </button>
                     </FormControl>
                     <FormMessage />
                   </FormItem>
@@ -176,19 +121,28 @@ export default function Login() {
                     </FormLabel>
                     <FormControl>
                       <Input
-                        type="password"
+                        type={showPassword ? "text" : "password"}
                         placeholder="Password"
                         {...field}
                         className="border-wbd-tertiary focus:ring-wbd-highlight focus:border-wbd-highlight"
                       />
+                      <button
+                        type="button"
+                        className="absolute right-3 top-1/2 transform -translate-y-1/2"
+                        onClick={() => setShowPassword(!showPassword)}
+                        aria-label="Toggle Password Visibility"
+                      >
+                        {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+                      </button>
                     </FormControl>
                     <FormMessage />
                   </FormItem>
                 )}
               />
               <Button
+                variant={"default"}
                 type="submit"
-                className="w-full mt-8 bg-wbd-primary text-white hover:bg-wbd-tertiary transition-all duration-300"
+                className="w-full mt-8"
               >
                 Login
               </Button>
